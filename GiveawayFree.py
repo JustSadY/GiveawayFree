0ed1094ce731ddc921f3526587d5e461c6ddfb17
JustSadY <bunyo2402@gmail.com>
2023-08-24 19:08:12 +0300

    Update GiveawayFree.py

--- GiveawayFree.py
+++ GiveawayFree.py
@@ -13,11 +13,10 @@ with open("Account.txt", "r") as keys:
     start_index_token = content.find("Token :")
     start_index_ID = content.find("ID :")
 
-    
     if start_index_email and start_index_token:
         start_index_email += len("Email :")
         end_index_email = content.find("\n", start_index_email)
-        
+
         start_index_Password += len("Password :")
         end_index_Password = content.find("\n", start_index_Password)
 
@@ -26,17 +25,17 @@ with open("Account.txt", "r") as keys:
 
         start_index_ID += len("ID :")
         end_index_ID = content.find("\n", start_index_ID)
-        
-        if end_index_email and end_index_token :
+
+        if end_index_email and end_index_token:
             email = content[start_index_email:end_index_email]
             password = content[start_index_Password:end_index_Password]
             TOKEN = content[start_index_token:end_index_token]
             id = int(content[start_index_ID:end_index_ID])
         else:
-            print("E-posta veya token hatalı biçimde yazılmış.")
+            print("hatalı biçimde yazılmış.")
             quit
     else:
-        print("E-posta veya token bulunamadı.")
+        print("Hesap bilgisi bulunamadı.")
         quit
 
 site_alian_url = "https://na.alienwarearena.com/ucf/Giveaway"
@@ -58,6 +57,7 @@ time.sleep(10)
 submit_alien.click()
 
 Bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
 
 @Bot.event
 async def on_ready():
@@ -93,11 +93,18 @@ async def on_ready():
                             By.XPATH, "/html/body/div[6]/span/p[1]"
                         ).text
                         await channel.send(comple)
-    
                     except:
-                        refuse = driver.find_element("Couldn't claim giveaway:", href)
-                        await channel.send(refuse)
+                        try:
+                            time.sleep(10)
+                            comple = driver.find_element(
+                                By.XPATH, "/html/body/div[6]/span/p[1]"
+                            ).text
+                            await channel.send(comple)
+                        except:
+                            await channel.send(href)
             brain = current_href_set
         else:
             print("Siteye ulaşılamadı.")
+
+
 Bot.run(TOKEN)
